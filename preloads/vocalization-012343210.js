446a5b672c9b623d3a35134ce62635d83967f311
JoVerVal <juak8.8@gmail.com>
2026-07-14 18:38:13 -0400

    Update vocalization-012343210.js

--- preloads/vocalization-012343210.js
+++ preloads/vocalization-012343210.js
@@ -2,8 +2,8 @@
 // description: Ascending-descending melodic vocalization with punchcard
 setcpm(12)
 $: n("[0,2,4]@2 ~ 0 1 2 3 4 3 2 1 0 ~ [0,2,4]@2 ~"
-     .add("<0 1 2 3 2 1 0>"
-          .add("<0 2 4 6 8 10 8 6 4 2 0>/7")))
+     .add("<0 1 2 3 2 1>"
+          .add("<0 2 4 6 8 10 8 6 4 2 0>/6")))
   .scale("C2: Major")
   .sound("piano")
   .color("magenta")
